500bbc1cc89e2529ad6da2ad6d3c8a152367c566
yaojianwei <yaojianwei@meicai.cn>
2016-09-06 18:50:46 +0800

    no message

---
+++ sina/sina/Classes/Home(首页)/View/CSTitleButton.h
@@ -0,0 +1,13 @@
+//
+//  CSTitleButton.h
+//  sina
+//
+//  Created by cain on 16/9/6.
+//  Copyright © 2016年 cain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSTitleButton : UIButton
+
+@end
---
+++ sina/sina/Classes/Home(首页)/View/CSTitleButton.m
@@ -0,0 +1,38 @@
+//
+//  CSTitleButton.m
+//  sina
+//
+//  Created by cain on 16/9/6.
+//  Copyright © 2016年 cain. All rights reserved.
+//
+
+#import "CSTitleButton.h"
+
+@implementation CSTitleButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+-(instancetype)initWithFrame:(CGRect)frame{
+    
+    return ;
+}
+
+-(void)layoutSubviews{
+    
+}
+
+-(void)setImage:(UIImage *)image forState:(UIControlState)state{
+    
+}
+
+-(void)setTitle:(NSString *)title forState:(UIControlState)state{
+    
+}
+
+@end
